98d3407720672f36d50e2b214b8862764ada5faf
Madhur-prog <bharambemadhur@gmail.com>
2026-09-17 10:24:09 +0530

    Time: 1 ms (99.93%), Space: 46.7 MB (91.93%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int n=nums.length;
+        int XOR=0;
+
+        for (int i=0;i<nums.length;i++){
+            XOR^=nums[i];
+
+
+
+        }
+        return XOR;
+        
+
+    }
+}
